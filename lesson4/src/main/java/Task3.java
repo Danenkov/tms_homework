b525e562481f1a807cd921c8c63d9396f69e7546
Danenkov <danenkovnikita@gmail.com>
2024-03-09 18:56:49 +0300

    lesson4

--- lesson4/src/main/java/Task3.java
+++ lesson4/src/main/java/Task3.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+
 public class Task3 {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,4,2,5,7,3,2};
+        int repeat = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (arr[i] == arr[j]) {
+                    repeat = arr[j];
+                }
+            }
+        }
+        System.out.println("Число встречающееся чаще всего - " + repeat);
+    }
 }
