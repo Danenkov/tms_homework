305ab7a2ca9524979bebddd2dcb4b0641695af30
Danenkov <danenkovnikita@gmail.com>
2024-03-12 18:29:58 +0300

    lesson4

--- lesson4/src/main/java/L4Task1.java
+++ lesson4/src/main/java/L4Task1.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Task1 {
+public class L4Task1 {
     public static void main(String[] args) {
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
--- lesson4/src/main/java/L4Task2.java
+++ lesson4/src/main/java/L4Task2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Task2 {
+public class L4Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите число элементов");
